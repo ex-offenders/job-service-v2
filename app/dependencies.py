b7fbcd97376300fb462a8fc7b5876c8b9f35b4bd
nvsajeeva <sajeeva@meta-carbon.com>
2024-07-05 14:28:43 +0100

    fix: debug authentication issue

--- app/dependencies.py
+++ app/dependencies.py
@@ -8,11 +8,16 @@ def get_session():
         yield session
 
 def get_user_id_from_token(request: Request) -> str:
+    token2 = request.headers.get("Authorization").split("Bearer ")[1]
+    print(token2)
+    payload2 = jwt.decode(token2, options={"verify_signature": False})
+    user_id2: str = payload2.get("sub")
+    print(payload2)
+    print(user_id2)
     try:
         token = request.headers.get("Authorization").split("Bearer ")[1]
         payload = jwt.decode(token, options={"verify_signature": False})
         user_id: str = payload.get("sub")
-        print(user_id)
         if user_id is None:
             raise HTTPException(status_code=401, detail="User ID not found in token")
         return user_id
